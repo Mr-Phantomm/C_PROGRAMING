ec711321989d67d77acccf7ca1b9f74f9ef34acb
Arun <arungarg57460@gmail.com>
2024-02-18 15:54:38 +0530

    daily

---
+++ 21.c
@@ -0,0 +1,6 @@
+#include<stdio.h>
+int main(){
+    if (5 && 5 ==5 ){
+        printf("true");
+    }
+}
---
+++ 22.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+    int a,b,i;
+    scanf("%d",&a);
+    for (i=2;i<=a;i++){
+        for (b=2;b<i;b++){
+            if 
+            {
+                printf("%d",i);
+            }
+        }
+        }
+        }
+ 
