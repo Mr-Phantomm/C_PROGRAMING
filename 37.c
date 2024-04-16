e4ff9b6edf55bb4958f1b7ae37e3b6c7bfe68c75
Arun <arungarg57460@gmail.com>
2024-04-16 11:48:20 +0530

    H

--- 37.c
+++ 37.c
@@ -1,1 +1,11 @@
-#inc
+#include<stdio.h>
+
+int main(){
+    struct num{
+        int real;
+        int comp;
+    }n1,n2;
+    scanf("%d%d%d%d",&n1.real,&n1.comp,&n2.real,&n2.comp);
+    printf("sum= %d+i%d",(n1.real+n2.real),(n1.comp+n2.comp));
+
+}
