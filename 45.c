2230fc5e3ab06160d69638d73c3a535aca9b1fac
Arun <arungarg57460@gmail.com>
2024-05-09 21:58:31 +0530

    daily

--- 45.c
+++ 45.c
@@ -1,7 +1,17 @@
-#include<stdio.h>
-int main(){
-    // int a=5;
-    int *ptr;
-    scanf("%d",ptr);
-    printf("%d",&ptr);
+// #include<stdio.h>
+// int main(){
+//     // int a=5;
+//     int *ptr;
+//     scanf("%d",ptr);
+//     printf("%d",&ptr);
+// }
+#include <stdio.h>
+#define CUBE(x) (x * x * x)
+void solve() {
+    int ans = 216 / CUBE(3);
+    printf("%d", ans);
+}
+int main() {
+    solve();
+	return 0;
 }
